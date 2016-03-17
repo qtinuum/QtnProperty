c82d73580033841059b99d42e85871bf1592ac2c
Alexandra Cherdantseva <neluhus.vagus@gmail.com>
2016-03-17 08:25:39 +0300

    initial CustomPropertyWidget

--- Extension/CustomPropertyWidget.cpp
+++ Extension/CustomPropertyWidget.cpp
@@ -5,37 +5,43 @@
 #include <QDrag>
 #include <QMimeData>
 
-static const QString kDragDropMimeType = "QtnPropertyDragDrop";
+static const QString kDragDropMimeType = "CustomPropertyDragDrop";
 
 CustomPropertyWidget::CustomPropertyWidget(QWidget *parent)
+	: QtnPropertyWidgetEx(parent)
 {
-	setAcceptDrops(true);
+//	setAcceptDrops(true);
 }
 
-void CustomPropertyWidget::mousePressEvent(QMouseEvent *event)
-{
-	if (event->button() == Qt::LeftButton)
-	{
-		auto view = propertyView();
+//void CustomPropertyWidget::mousePressEvent(QMouseEvent *event)
+//{
+//	if (event->button() == Qt::LeftButton)
+//	{
+//		auto view = propertyView();
 
-		auto property = view->getPropertyAt(event->pos());
+//		auto property = view->getPropertyAt(event->pos());
 
-		if (nullptr != property
-		&&	0 != (property->stateLocal() & QtnPropertyStateDraggable))
-		{
+//		if (nullptr != property
+//		&&	0 != (property->stateLocal() & QtnPropertyStateDraggable))
+//		{
 
-		}
-	}
-}
+//		}
+//	}
+//}
+
+//void CustomPropertyWidget::moseMoveEvent(QMouseEvent *event)
+//{
+
+//}
 
 
-void CustomPropertyWidget::dragEnterEvent(QDragEnterEvent *event)
-{
+//void CustomPropertyWidget::dragEnterEvent(QDragEnterEvent *event)
+//{
 
-}
+//}
 
-void CustomPropertyWidget::dropEvent(QDragEnterEvent *event)
-{
-	if (event->mimeData()->hasFormat(kDragDropMimeType))
-		event->acceptProposedAction();
-}
+//void CustomPropertyWidget::dropEvent(QDragEnterEvent *event)
+//{
+//	if (event->mimeData()->hasFormat(kDragDropMimeType))
+//		event->acceptProposedAction();
+//}
--- Extension/CustomPropertyWidget.h
+++ Extension/CustomPropertyWidget.h
@@ -2,15 +2,16 @@
 
 #include "PropertyWidgetEx.h"
 
-class CustomPropertyWidget : public QtnPropertyWidget
+class CustomPropertyWidget : public QtnPropertyWidgetEx
 {
 	Q_OBJECT
 
 public:
 	explicit CustomPropertyWidget(QWidget *parent = nullptr);
 
-protected:
-	virtual void mousePressEvent(QMouseEvent *event) override;
-	virtual void dragEnterEvent(QDragEnterEvent *event) override;
-	virtual void dropEvent(QDragEnterEvent *event) override;
+//protected:
+//	virtual void mousePressEvent(QMouseEvent *event) override;
+//	virtual void moseMoveEvent(QMouseEvent *event) override;
+//	virtual void dragEnterEvent(QDragEnterEvent *event) override;
+//	virtual void dropEvent(QDragEnterEvent *event) override;
 };
